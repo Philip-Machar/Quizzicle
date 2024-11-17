5190eb9fefcde98a43ed52acb41cfa1a4cf9c39f
Philip-Machar <philipmachar788@gmail.com>
2024-11-17 10:57:25 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <main className="w-[600px] h-[90vh] bg-[#d9e2fd] shadow-xl rounded-lg">
+      
+    </main>
+  )
+}
+
+export default App
