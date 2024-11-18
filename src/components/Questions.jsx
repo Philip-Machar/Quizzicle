4d2975c7b1d9b4e2f5890656015a65c7c7bfc1f1
Philip-Machar <philipmachar788@gmail.com>
2024-11-18 16:07:47 +0300

    Complete converting htlm entities

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,50 +1,56 @@
+import { useState, useEffect } from "react"
+
 const Questions = () => {
-  return (
-    <div className="flex flex-col gap-8">
-        <div className="flex flex-col gap-10 px-8">
-          <div className="text-lg">
-            <div className="font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, quasi!</div>
-            <div className="flex gap-4 mt-3">
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer hover:bg-[#c1c4fc] hover:border-transparent">Hola</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer hover:bg-[#c1c4fc] hover:border-transparent">Bonjour</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer hover:bg-[#c1c4fc] hover:border-transparent">Ciao</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer hover:bg-[#c1c4fc] hover:border-transparent">Namaste</div>
+    const [questions, setQuestions] = useState([])
+
+    //Decoding HTML entities
+    const decodeHtml = (html) => {
+        const textArea = document.createElement("textarea");
+        textArea.innerHTML = html;
+        return textArea.value;
+    }
+
+    useEffect(() => {
+        const getQuestions = async () => {
+            try {
+                const res = await fetch("https://opentdb.com/api.php?amount=5");
+                const data = await res.json();
+                //Deconde questions and answers
+                const decodedQuestions = data.results.map((question) => ({
+                    ...question,
+                    question: decodeHtml(question.question),
+                    correct_answer: decodeHtml(question.correct_answer),
+                    incorrect_answers: question.incorrect_answers.map(incorrect_answer => decodeHtml(incorrect_answer)),
+                }));
+
+                setQuestions(decodedQuestions);
+            } catch (error) {
+                console.error(`Error: ${error}`)
+            }
+        };
+        getQuestions();
+    }, []);
+
+      // Conditional rendering to check if questions are available
+    if (questions.length === 0) {
+        return <div>Loading...</div>; // Show loading message until questions are fetched
+    }
+
+    return (
+        <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-10 px-8">
+                <div className="text-lg">
+                    <div className="font-bold">{questions[0].question}</div>
+                    <div className="flex gap-4 mt-3">
+                    <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer hover:bg-[#c1c4fc] hover:border-transparent">Hola</div>
+                    <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer hover:bg-[#c1c4fc] hover:border-transparent">Bonjour</div>
+                    <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer hover:bg-[#c1c4fc] hover:border-transparent">Ciao</div>
+                    <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer hover:bg-[#c1c4fc] hover:border-transparent">Namaste</div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="text-lg">
-            <div className="font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem facere aperiam modi!</div>
-            <div className="flex gap-4 mt-3">
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Hola</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Bonjour</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Ciao</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Namaste</div>
-            </div>
-          </div>
-          <div className="text-lg">
-            <div className="font-bold">Lorem ipsum dolor sit, amet consectetur adipisicing elit Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat neque eum omnis!.</div>
-            <div className="flex gap-4 mt-3">
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Hola</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Bonjour</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Ciao</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Namaste</div>
-            </div>
-          </div>
-          <div className="text-lg">
-            <div className="font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis nihil ad dolorum sint quasi ut.</div>
-            <div className="flex gap-4 mt-3">
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Hola</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Bonjour</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Ciao</div>
-              <div className="min-w-12 border-2 border-[#000] py-1 px-2 rounded-xl grid place-content-center cursor-pointer">Namaste</div>
-            </div>
-          </div>
         </div>
-        <div className="w-full flex justify-center items-center gap-4">
-          <div className="font-bold">You have scored 3/5 correct</div>
-          <button className="bg-[#465090] py-2 px-4 rounded-md min-w-12 text-white">Check Answers</button>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Questions
