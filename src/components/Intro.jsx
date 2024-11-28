fcdb9cd3909aef077e7b07edab43ad3582c26cee
Philip-Machar <philipmachar788@gmail.com>
2024-11-28 21:55:17 +0300

    Final commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const Intro = () => {
   return (
-    <div className="flex flex-col gap-2 justify-center text-center px-24">
+    <div className="flex flex-col gap-2 justify-center items-center text-center px-24">
         <h1 className="text-3xl text-[#465090] font-bold">Quizzicle</h1>
-        <p>Test your knowledge and challenge yourself with this fun multiple-choice quiz. Are you ready to begin?</p>
+        <p className="w-[50%]">Test your knowledge and challenge yourself with this fun multiple-choice quiz. Are you ready to begin?</p>
         <Link to="/questions">
             <button className="bg-[#465090] py-2 px-4 rounded-md min-w-12  text-white">Start Quiz</button>
         </Link>
